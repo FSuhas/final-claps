48c6bee97c8487f3c88a15b8f918fceb545ccd52
fabien suhas <fsuhas@gmx.com>
2022-10-20 19:31:19 +0200

    heroku config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.action_mailer.default_url_options = { host: 'final-claps.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_caching = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'final-claps.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    server: "smtp.gmail.com",
+    port: 587,
+    user_name: "fs6400@gmail.com",
+    password: ENV['GMX_PASSWORD'],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    ssl: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
