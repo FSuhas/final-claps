9d0598e0a7d1d6b907c2969f3ee6cb4e22379f4f
fabien suhas <fsuhas@gmx.com>
2022-10-21 13:39:36 +0200

    sendgrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -9,17 +9,6 @@ class ApplicationMailer < ActionMailer::Base
 
   def confirmation_instructions(user)
     @user = user
-    from = SendGrid::Email.new(email: 'fsuhas@gmx.com')
-    to = SendGrid::Email.new(email: @user.email)
-    subject = 'Confirmation instructions'
-    content = SendGrid::Content.new(type: 'text/plain', value: attachments['confirmation_instructions.html.erb'])
-    mail = SendGrid::Mail.new(from, subject, to, content)
-
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    puts response.parsed_body
-    puts response.headers
+    mail(to: @user.email, subject: "Confirmation instructions", template_path: 'devise/mailer', template_name: 'confirmation_instructions')
   end
 end
