7b413b63100a5b96846be816d9c0bdcdda0087a6
fabien suhas <fsuhas@gmx.com>
2022-10-21 12:07:30 +0200

    sendgrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -6,23 +6,23 @@ class ApplicationMailer < ActionMailer::Base
   default from: "fsuhas@gmx.com"
   layout 'mailer'
 
+  from = SendGrid::Email.new(email: 'fsuhas@gmx.com')
+  to = SendGrid::Email.new(email: 'fsuhas@gmx.com')
+  subject = 'Sending with SendGrid is Fun'
+  content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+  mail = SendGrid::Mail.new(from, subject, to, content)
 
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  puts response.status_code
+  puts response.body
+  puts response.parsed_body
+  puts response.headers
 
 
   def confirmation_instructions(user)
     @user = user
     mail(to: user.email, subject: "Please confirm your account", template_path: "devise/mailer", template_name: "confirmation_instructions")
-    from = SendGrid::Email.new(email: 'test@exemple.com')
-    to = SendGrid::Email.new(email: 'test@expemple.com')
-    subject = 'Sending with SendGrid is Fun'
-    content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-    mail = SendGrid::Mail.new(from, subject, to, content)
 
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    puts response.parsed_body
-    puts response.headers
   end
 end
