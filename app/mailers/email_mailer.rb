77a8d3504a0fb038284b6a642f94d3517b01e424
fabien suhas <fsuhas@gmx.com>
2022-10-23 12:50:27 +0200

    sendgrid

--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -2,8 +2,7 @@ class EmailMailer < ApplicationMailer
 
   def confirmation_mail(user)
     @user = user
-    @resource = user
-    @token = @user.confirmation_token
+    @token = user.confirmation_token
     mail(to: @user.email, subject: 'Confirmation email')
   end
 end
