b33e985072a07e50ff83cf3fe4ff107578eda0d4
fabien suhas <fsuhas@gmx.com>
2022-10-24 17:35:44 +0200

    sendgrid on create user custom

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { users: 'registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations', confirmations: 'users/confirmations' }
 
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
