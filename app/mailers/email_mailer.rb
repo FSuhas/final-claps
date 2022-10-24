939948d2156d5081430775a854f2b0a63255971c
fabien suhas <fsuhas@gmx.com>
2022-10-24 14:58:02 +0200

    sendgrid

--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -9,17 +9,16 @@ class EmailMailer < ApplicationMailer
     @token = user.confirmation_token
     mail(to: @user.email, subject: 'Confirmation email')
 
-  from = Email.new(email: 'fsuhas@gmx.com')
-  to = Email.new(email: @user.email)
-  subject = 'Corfirmation email'
-  content = Content.new(type: 'text/plain',
-    value: '<p>Bienvenue <%= @email %> !</p><p>Vous pouvez confirmer l adresse e-mail de votre compte via le lien ci-dessous :</p><p><%= link_to "Confirmer mon compte", confirmation_url(@user, confirmation_token: @token) %></p>')
-  mail = Mail.new(from, subject, to, content)
+    from = Email.new(email: 'fsuhas@gmx.com')
+    to = Email.new(email: @user.email)
+    subject = 'Confirmation email'
+    content = Content.new(type: 'text/plain', template_path: 'email_mailer/confirmation_mail.html.erb')
+    mail = Mail.new(from, subject, to, content)
 
-  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-  response = sg.client.mail._('send').post(request_body: mail.to_json)
-  puts response.status_code
-  puts response.body
-  puts response.headers
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.headers
   end
 end
