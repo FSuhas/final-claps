adfef19e9e7d8441dd2bfc6e8e6f6e09e487d117
fabien suhas <fsuhas@gmx.com>
2022-10-30 08:40:39 +0100

    home front

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,24 @@
 class JobsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+    @news = Newsletter.new
+
+    if user_signed_in?
+      @user = current_user
+    else
+      @user = User.new
+    end
+
+    if params[:query].present?
+      @jobs = Job.search_by_query(params[:query])
+      @jobs = @jobs.where("date >= ?", Time.now).order(date: :asc)
+    else
+      @jobs = Job.all
+      @jobs = @jobs.where("date >= ?", Time.now).order(date: :asc)
+    end
+  end
+
   def new
     @user = current_user
     @job = Job.new
